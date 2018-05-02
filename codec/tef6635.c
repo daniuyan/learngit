db8457e89dca5e7e3abd3b2821d445626e99660f
test <test@ubuntutest.test>
2018-05-02 20:31:56 +0800

    modified the name as the machine defined

--- codec/tef6635.c
+++ codec/tef6635.c
@@ -1,5 +1,5 @@
 
-/* 1. 构造一个snd_soc_dai_driver   tef6635_codec_driver
+/* 1. 构造一个snd_soc_dai_driver   tef6635_dai
  * 2. 构造一个snd_soc_codec_driver  tef6635_codec_driver
  * 3. 注册它们
  */
@@ -11,7 +11,7 @@ static const struct snd_soc_dai_ops tef6635_ops = {
 };
 
 static struct snd_soc_dai_driver tef6635_dai = {
-	.name = "tef6635_dai",
+	.name = "tef6635_codec_dai",
 	.playback = {
 		.stream_name = "Playback",
 		.channels_min = 2,
@@ -20,13 +20,6 @@ static struct snd_soc_dai_driver tef6635_dai = {
 		 * only support 8~48K + 96K,
 		 * TODO modify hw_param to support more
 		 */
-		.rates = SNDRV_PCM_RATE_8000_48000 | SNDRV_PCM_RATE_96000,
-		.formats = TEF6635_FORMATS,
-	},
-	.capture = {
-		.stream_name = "Capture",
-		.channels_min = 2,
-		.channels_max = 2,
 		.rates = SNDRV_PCM_RATE_8000_48000 | SNDRV_PCM_RATE_96000,
 		.formats = TEF6635_FORMATS,
 	},
@@ -52,7 +45,7 @@ static int uda1341_remove(struct platform_device *pdev)
 
 struct platform_driver tef6635_drv = {
 	.driver = {
-	   .name = "tef6635",
+	   .name = "tef6635_codec",
 	   .owner = THIS_MODULE,
 	},
 	.probe = tef6635_probe,		//待补充
@@ -67,7 +60,7 @@ static void tef6635_dev_release(struct device * dev)
 }
 
 static struct platform_device tef6635_dev = {
-    .name         = "tef6635-codec",
+    .name         = "tef6635_codec",
     .id       = -1,
     .dev = { 
     	.release = tef6635_dev_release, 
